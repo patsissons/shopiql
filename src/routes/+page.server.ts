c62d963c7b28989014cee68df717b4b4bf1e924f
Pat Sissons <patricksissons@gmail.com>
2023-03-17 16:26:30 -0700

    auto-redirect to graphiql

---
+++ src/routes/+page.server.ts
@@ -0,0 +1,6 @@
+import { redirect } from '@sveltejs/kit'
+import type { PageServerLoad } from './$types'
+
+export const load = (() => {
+  throw redirect(302, '/api/graphql')
+}) satisfies PageServerLoad
