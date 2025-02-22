624b4433653f1400980684f1e1ff9816d4d5b3a4
Pat Sissons <patricksissons@gmail.com>
2025-02-22 15:52:42 -0800

    adding pages support

--- src/lib/server/graphql/modules/index.ts
+++ src/lib/server/graphql/modules/index.ts
@@ -1,8 +1,9 @@
 import { Collection } from './Collection'
+import { Page } from './Page'
 import { Product } from './Product'
 import { schema } from './schema'
 import { search } from './search'
 import { Shop } from './Shop'
 
 // for more endpoints, see: https://github.com/shapedigital/shopify-resources/wiki/Public-Endpoints
-export const modules = [schema, Shop, Product, Collection, search]
+export const modules = [schema, Shop, Product, Collection, Page, search]
